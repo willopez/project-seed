9eb8da6eb4304d04f5a78bb443005bbe651d50c1
willopez <will@noomadigital.com>
2017-09-29 19:28:51 -0700

    Add Footer component.

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
+import Footer from './Footer';
 
 const Layout = props => (
   <div className="cover-container">
@@ -8,6 +9,7 @@ const Layout = props => (
     <main role="main" className="inner cover">
       {props.children}
     </main>
+    <Footer />
   </div>
 );
 
